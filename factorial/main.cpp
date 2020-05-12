6358036d37de74cff30c943e3d1cf93d74c0cca0
alejoa93 <ra.abello1021@uniandes.edu.co>
2020-05-12 17:55:00 -0400

    archivo de factoriales

---
+++ factorial/main.cpp
@@ -0,0 +1,35 @@
+/*
+    Tarea de hoy:
+1) Clonen en el servidor facciencias.uniandes.edu.co/jhub/ su repositorio de cossas de git de la clase pasada
+
+2) Ingresen al repositorio clonado
+
+3)  van a crear un programa en C++ que calcule calcule la función factorial de un número entero que ustedes mismos ingresen en la terminal
+4) Subir el archivo llamado "factorial.cpp" al repositorio.
+5) entregar el link del archivo en el repositorio.
+*/
+
+#include<iostream>
+using namespace std;
+
+int factorial(int a){
+    int i = 1;
+    int f = 1;
+    while(i <= a){
+        f *= i;
+        i ++;
+    }
+    return f;
+}
+
+int main(){
+    cout << "Obtengamos el factorial" << endl;
+    int n;
+    cout << "Por favor ingrese un número entero"<<endl;
+    cin >> n;
+    cout << "El número es: " << n << endl;
+    
+    cout << n << "! = "<< factorial(n) <<endl;
+    return 0;
+}
+
